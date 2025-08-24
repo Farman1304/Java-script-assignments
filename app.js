4d8138cf8b153c09443832f6280d63d1d5c36349
Farman1304 <farmanullah1107@gmail.com>
2025-08-24 08:10:25 -0700

    assignment done

---
+++ app.js
@@ -0,0 +1,103 @@
+// // Chapter # 1
+
+// //  Q.1
+
+// alert ('Welcom to website');
+// //  Q.2
+// alert('Error! Please enter a valid password');
+// //  Q.3
+// alert( ' welcome to JS land.... \n Happy coding.')
+// //  Q.4
+
+// alert ('welcom to JS land');
+
+// alert ('Happy Coding! \n Prevent this page from creating addtitional dialogs');
+
+// //  Q.5
+
+// console.log('Hello . . . I can run JS through my web browser`s console');
+
+// // chapter # 2
+
+// var userName;
+
+// var myName = 'Farman Ullah';
+
+// var message= '';
+
+// message = 'Hello World';
+
+// alert(userName)
+// alert(message)
+
+// var age = 33;
+
+// var course = 'web and mobile application development';
+
+// alert(myName + ' ' + age + ' '+ 'years old and I am learning' +  course);
+
+// alert ('Pizza \n pizz \n piz \n pi \n p');
+
+// var myEmail = 'farmanullahyousufxai@gmail.com';
+
+// alert('My email is ' + myEmail);
+
+// var book = 'the smarter way to learn java script';
+
+// alert (book);
+    
+// document.write ('Yah! I can write HTML content through Java script  <br>');
+
+// // chapter # 3
+
+// var myAge = 33;
+// alert('i am ' + myAge + 'years old');
+
+
+// var noOfcustomervisit = 14;
+
+// alert ('you have visited ' + noOfcustomervisit + 'this site');
+
+
+// var birthYear = 1991;
+
+// document.write ('My birth year is  ' + birthYear + '<br>' + 'data type of declear variable is ' + typeof(birthYear) + '<br>' );
+
+
+// var visiter = 'John deo';
+// var productTitile = 'shirts'
+// var Qty = 5;
+
+
+
+// document.write (' \n Mr.' + visiter + ' ordered ' + Qty + ' from XYZ store <br>');
+
+
+
+// //  chapter # 4
+
+// var school , location , Id;
+
+// var myName;
+// var $price;
+// var _age;
+// var user123;
+// var first_Name;
+
+
+// var 123user;    
+// var user name;  
+// var my-name;    
+// var @email;     
+// var let; 
+
+
+
+// chapter # 5
+
+var num1 = 6;
+var num2 = 11;
+
+document.write ('The sum of ' + num1 +' and ' + num2 +' is ' + ( num1 + num2) +'<br>');
+
+
